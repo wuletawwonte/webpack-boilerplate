d5653306b9b682e30ac614c22e4c6dd4d37ffbaa
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-23 19:01:00 +0300

    Add style.css file and configure it in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: "./src/index.html",
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
-
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
